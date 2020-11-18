16cdd277fe8621cd48fb6381587ac8d86e84b263
clement piot <c.piot@me.com>
2020-11-18 16:58:46 +0100

    100/100

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -24,7 +24,6 @@ class RestaurantsController < ApplicationController
 private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category, :stars)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
-
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian']
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
