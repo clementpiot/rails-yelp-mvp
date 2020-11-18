260b7c028908686db0e8aa05c71c7e184245e382
clement piot <c.piot@me.com>
2020-11-18 16:36:24 +0100

    le CSS n'est pas ouf mais ca passe

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian']
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create!(
+  name: 'La Chicorée',
+  address: 'Place Rihour, Lille',
+  phone_number: '06 06 06 89 68',
+  category: 'French',
+)
+
+Restaurant.create!(
+  name: 'La Bottega',
+  address: 'Rue Saint André, Lille',
+  phone_number: '06 17 87 89 01',
+  category: 'Italian',
+)
+
+Restaurant.create!(
+  name: 'La Brocantine',
+  address: 'Place du marché, Cysoing',
+  phone_number: '06 01 13 89 43',
+  category: 'Belgian',
+)
+
+Restaurant.create!(
+  name: 'Sakura',
+  address: 'Place du Marché Saint Honoré, Paris',
+  phone_number: '06 12 15 16 00',
+  category: 'Chinese',
+)
+
+Restaurant.create!(
+  name: 'Chez Simone',
+  address: 'Rue du Pain Bénit, Marseille',
+  phone_number: '04 06 45 85 66',
+  category: 'French',
+)
